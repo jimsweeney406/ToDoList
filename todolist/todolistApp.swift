45799e0e56766ac6cc00dc77ef28be0e5a98c253
Jim Sweeney <jim.sweeney.ios@gmail.com>
2025-03-10 18:40:22 -0400

    second commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by James Sweeney on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
